0f97ad165a29a4185b7ef6e45f0c753a7b6d3795
Shebeli <Shervinsaeedi78@gmail.com>
2020-07-19 16:16:23 +0430

    added SiteWallet and WalletTransactions model to models.py

--- food/models.py
+++ food/models.py
@@ -1,5 +1,7 @@
 from django.db import models
+from django.conf import settings
 from django.contrib.auth.models import AbstractUser
+
 
 class SiteUser(AbstractUser):
     first_name = models.CharField(max_length=30,blank=True,verbose_name='نام')
@@ -19,4 +21,30 @@ class TheFood(models.Model):
     def __str__(self):
         return f"{self.name} --> {self.price}"
 
+class SiteWallet(models.Model):
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    balance = models.IntegerField(verbose_name='حساب',default=0)
+
+    def __str__(self):
+        return f"{self.owner}: {self.balance}$"
+
+    def deposit(self, amount):
+        self.balance += amount
+        WalletTransactions.objects.create(amount=amount, new_balance=self.balance,kind='DP')
+        return self.balance
+
+    def withdraw(self, amount):
+        if self.balance >= amount:
+            self.balance -= amount
+            WalletTransactions.objects.create(amount=amount, new_balance=self.balance,kind='WD')
+            return self.balance
+        return None
+
+class WalletTransactions(models.Model):
+    amount = models.IntegerField()
+    new_balance = models.IntegerField()
+    date = models.DateTimeField(auto_now_add=True)
+    kind = models.TextField(choices=[('DP','واریز'),('WD','برداشت')])
+
+    
 
