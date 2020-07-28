c16b9d18696c713a1a05fedc86fbaa3316380d24
Shebeli <Shervinsaeedi78@gmail.com>
2020-07-28 13:24:16 +0430

    changed settings

--- ghaza/settings.py
+++ ghaza/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+from .secret import *
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 't3#s8!e*c*&q4lzzp__*i!zz2c(o(88yvi0zr)b=-w)g@jkw7='
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
