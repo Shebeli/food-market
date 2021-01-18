0b41903a51bf47cf9e374eecf71ee02fcc94de8e
Shebeli <Shervinsaeedi78@gmail.com>
2021-01-19 00:14:01 +0330

    updatetddd

--- ghaza/settings.py
+++ ghaza/settings.py
@@ -208,7 +208,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'staticfiles'),
+    os.path.join(BASE_DIR, 'staticfiles'),
 )
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
