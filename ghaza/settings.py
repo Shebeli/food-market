af416f6e1e32d4785c96b9e49e7f9134a86bdfd4
Shebeli <Shervinsaeedi78@gmail.com>
2021-01-18 17:08:45 +0330

    updated settings for heroku deploy 2

--- ghaza/settings.py
+++ ghaza/settings.py
@@ -204,7 +204,7 @@ else:
     except NameError:
         pass
 
-PROJECT_ROOT = os.path.join(os.path.abspath(__file__))
+PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 
 STATICFILES_DIRS = (
